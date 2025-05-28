e3b75ddac1e3375bdccc0b9b0fb1cf4fc6737d49
Badrshaba <badrshaban1212@gmail.com>
2025-05-29 00:18:02 +0300

    feat(setup):routing and folder structure

--- super-fitness/src/main.tsx
+++ super-fitness/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RootLayout from "./app/layout.tsx";
-import HomePage from "./app/page.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route index element={<HomePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
