7664b66b8152b594a2549cf0e9ecb2d26961fc69
reemazakout <reemazakout191@gmail.com>
2025-07-04 20:34:44 +0300

    (otp-flow):Implemented otp form, forget password and create new password

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home, Login, Register, Layout } from "../app/index";
+import { Home, Login, Register, Layout, OtpCodeForm, ForgetPassword } from "../app/index";
 import ProdectRoute from "../components/common/ProdectRoute";
 import { Suspense } from "react";
 import PagesLoader from "@/components/common/pages-loader/pages-loader";
+import CreateNewPassword from "@/components/features/components/create-new-password";
 
 export const routes = createBrowserRouter([
   {
@@ -22,13 +23,38 @@ export const routes = createBrowserRouter([
     ),
   },
   {
-    path: "/login",
+    path: "/otp-code",
+    element: (
+      <Suspense fallback={<PagesLoader />}>
+        <OtpCodeForm />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/forget-password",
+    element: (
+      <Suspense fallback={<PagesLoader />}>
+        <ForgetPassword />
+      </Suspense>
+    ),
+  },
+
+  {
+    path: "/create-New-Password",
+    element: (
+      <Suspense fallback={<PagesLoader />}>
+        <CreateNewPassword />
+      </Suspense>
+    ),
+  },
+
+  {
+    path: "/",
     element: (
       <Suspense fallback={<PagesLoader />}>
         <Layout />
       </Suspense>
     ),
-
     children: [
       {
         index: true,
